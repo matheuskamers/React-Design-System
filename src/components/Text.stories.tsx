0d757f7fc3eccf6c6a1d6af2496ba99e3d8a2fe9
matheuskamers <mkamersandrade@gmail.com>
2022-10-13 21:54:03 -0300

    Add components and story book

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,59 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Text, TextProps } from "./Text";
+
+export default {
+   title: 'Components/Text',
+   component: Text,
+   args: {
+      children: 'Text Anything',
+      size: 'md',
+   },
+   argTypes: {
+      size: {
+         options: ['sm', 'md', 'lg'],
+         control : {
+            type: 'inline-radio'
+         }
+      },
+      asChild: {
+         table: {
+            disable: true,
+         }
+      }
+   }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+   args: {
+      size: 'sm'
+   }
+}
+
+export const Large: StoryObj<TextProps> = {
+   args: {
+      size: 'lg'
+   }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+   args: {
+      asChild: true,
+      children: (
+         <p>Text with P tag</p>
+      )
+   },
+   argTypes: {
+      children: {
+         table: {
+            disable: true,
+         }
+      },
+      asChild: {
+         table: {
+            disable: true,
+         }
+      }
+   }
+}
